a5877a743d9cd7f6589b0c216c70c2371fbbbef0
Maksym Mikhieiev <mih123eeev@gmail.com>
2023-10-16 21:19:26 +0300

    Chars counter

---
+++ app/src/main/java/com/example/madt_2_lab_wordcounter/CounterWord.java
@@ -0,0 +1,8 @@
+package com.example.madt_2_lab_wordcounter;
+
+public class CounterWord {
+    public static int getCharsCount(String phrase)
+    {
+        return phrase.length();
+    }
+}
---
+++ app/src/main/java/com/example/madt_2_lab_wordcounter/MainActivity.java
@@ -0,0 +1,55 @@
+ package com.example.madt_2_lab_wordcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+ public class MainActivity extends AppCompatActivity {
+
+    private EditText edTextInput;
+    private TextView tvResult;
+    private Spinner spinner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.edTextInput=findViewById(R.id.edTextInput);
+        this.tvResult=findViewById(R.id.tvResult);
+
+        this.spinner = (Spinner) findViewById(R.id.spCountOption);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.selection_options, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.spinner.setAdapter(adapter);
+    }
+
+
+     public void btnCountClick(View view) {
+         String selectionChars = getResources().getString(R.string.selection_chars);
+
+         if (this.spinner.getSelectedItem().toString().equalsIgnoreCase(selectionChars)) {
+
+             String TextInput = this.edTextInput.getText().toString();
+             Log.i("UserText", TextInput);
+
+             int count = CounterWord.getCharsCount(TextInput);
+             Log.i("UserTextCount", String.valueOf(count));
+
+             this.tvResult.setText(String.valueOf(count));
+         }
+         else {
+             Toast.makeText(this,"Not Implemented", Toast.LENGTH_SHORT).show();
+
+         }
+     }
+ }
